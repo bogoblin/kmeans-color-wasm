44d1293ea4de9fe9f71640cf025cc7cd8f2c0061
bobby <bogoblin@gmail.com>
2024-10-17 13:57:26 +0100

    Make everything look nice

--- palette-demo/src/App.tsx
+++ palette-demo/src/App.tsx
@@ -20,9 +20,49 @@ function App() {
     const timeTaken = performance.now() - benchmarkStart;
 
     return (
-        <form className={"flex flex-col"}>
-            <h1 className={"text-4xl font-bold m-3 text-center flex-grow-0" +
-                ""}>Create Palette from Image</h1>
+        <form className={"flex flex-col bg-base-100"}>
+            <div className={"navbar"}>
+                <div className={"flex-1"}>
+                    <span className={"btn btn-ghost text-xl"}>
+                         k-means palette generator
+                    </span>
+                </div>
+                <div className={"flex-none"}>
+                    <ul className={"menu menu-horizontal"}>
+                        <li><a className={"btn btn-ghost btn-circle"}
+                               href={"https://www.npmjs.com/package/kmeans-color-wasm"}><img className={"w-1/2"}
+                                                                                             src={"https://raw.githubusercontent.com/npm/logos/refs/heads/master/npm%20square/n.svg"}/></a>
+                        </li>
+                        <li><a className={"btn btn-ghost btn-circle"}
+                               href={"https://github.com/bogoblin/kmeans-color-wasm"}><img
+                            className={"w-1/2 bg-neutral-content mask mask-circle"}
+                            src={"https://raw.githubusercontent.com/gilbarbara/logos/refs/heads/main/logos/github-icon.svg"}/></a>
+                        </li>
+                    </ul>
+                </div>
+                <label className="swap swap-rotate">
+                    {/* this hidden checkbox controls the state */}
+                    <input type="checkbox" className="theme-controller" value="light"/>
+
+                    {/* sun icon */}
+                    <svg
+                        className="swap-off h-10 w-10 fill-current"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24">
+                        <path
+                            d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z"/>
+                    </svg>
+
+                    {/* moon icon */}
+                    <svg
+                        className="swap-on h-10 w-10 fill-current"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24">
+                        <path
+                            d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z"/>
+                    </svg>
+                </label>
+            </div>
             <div className={"min-h-0 relative max-h-full flex-grow"}>
                 <input className={"absolute opacity-0 w-full h-full z-20 cursor-pointer"}
                        id={"imageUpload"}
@@ -33,7 +73,7 @@ function App() {
                         setImageUrl(URL.createObjectURL(file));
                     }
                 }}/>
-                <div className={"absolute w-full h-full bg-black flex"}
+                <div className={"absolute w-full h-full bg-base-100 flex"}
                 >
                     <div className={"text-center flex-grow " +
                         " m-10 p-10 border-8 border-dashed rounded-3xl border-opacity-60 " +
@@ -54,8 +94,8 @@ function App() {
                 </label>
             </div>
             <Palette palette={palette}/>
-            <p className={"p-2 text-right italic flex-grow-0"}>done in {timeTaken.toFixed(2)}ms</p>
             <Controls kMeansOptions={kMeansOptions} setKMeansOptions={setKMeansOptions}/>
+            <p className={"p-2 text-right italic flex-grow-0"}>done in {timeTaken.toFixed(2)}ms</p>
         </form>
     )
 }
@@ -65,12 +105,12 @@ function Controls({kMeansOptions, setKMeansOptions}: {
     setKMeansOptions: (o: GetKmeansOptions) => void
 }) {
     const exampleCode = `const centroids = get_kmeans(image, ${JSON.stringify(kMeansOptions, null, 4)});`;
-    return <div className={"card bg-secondary shadow-xl flex-row m-4 justify-between"}
+    return <div className={"card bg-base-300 shadow-xl flex-row m-4 justify-between"}
     >
         <div className={"card-body flex-row w-full justify-between"}>
             <div>
                 <label htmlFor={"k"} className={"label"}>
-                    <span className={"label-text text-secondary-content"}>Clusters:</span>
+                    <span className={"label-text"}>Clusters:</span>
                     <input className={"input max-w-24"}
                            id={"k"}
                            type={"number"} value={kMeansOptions.k}
@@ -82,7 +122,7 @@ function Controls({kMeansOptions, setKMeansOptions}: {
                            }}/>
                 </label>
                 <label htmlFor={"max_iterations"} className={"label gap-4"}>
-                    <span className={"label-text text-secondary-content"}>Iterations:</span>
+                    <span className={"label-text"}>Iterations:</span>
                     <input className={"input max-w-24"}
                            id={"max_iterations"}
                            type={"number"} value={kMeansOptions.max_iter}
@@ -96,7 +136,7 @@ function Controls({kMeansOptions, setKMeansOptions}: {
                 </label>
                 <div className={""}>
                     <label className={"label justify-center gap-2"}>
-                        <span className={"label-text text-secondary-content"}>RGB</span>
+                        <span className={"label-text"}>RGB</span>
                         <input type={"checkbox"} className={"toggle"}
                                onChange={e => {
                                    const setToLab = e.target.checked;
@@ -109,11 +149,11 @@ function Controls({kMeansOptions, setKMeansOptions}: {
                                    setKMeansOptions(options);
                                }}
                         />
-                        <span className={"label-text text-secondary-content"}>LAB</span>
+                        <span className={"label-text"}>LAB</span>
                     </label>
                 </div>
             </div>
-            <div className={"rounded p-4 bg-black bg-opacity-50 text-accent-content"}>
+            <div className={"overflow-x-scroll rounded-lg p-4 bg-black bg-opacity-50"}>
                 {exampleCode.split('\n').map(line => <pre key={line}><code data-prefix={""}>{line}</code></pre>)}
             </div>
         </div>
@@ -138,7 +178,7 @@ function Palette({palette}: { palette: Centroid[] }) {
 
 function Copiable({text}: {text: string}) {
     return <div className={"p-0.5"}>
-        <a className={"cursor-pointer w-full bg-gray-900 p-0.5 m-1 rounded"} onClick={() => navigator.clipboard.writeText(text)}>
+        <a className={"cursor-pointer bg-gray-900 text-white text-opacity-80 p-0.5 m-1 rounded"} onClick={() => navigator.clipboard.writeText(text)}>
             {text}
         </a>
     </div>;
